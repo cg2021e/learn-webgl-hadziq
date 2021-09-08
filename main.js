00454db72580a97d059645d2bbd50dba2bdcab3f
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-08 14:51:12 +0700

    Fill the background with dark grey

---
+++ main.js
@@ -0,0 +1,41 @@
+function main() {
+    //Access the canvas through DOM: Document Object Model
+    var canvas = document.getElementById('myCanvas');   // The paper
+    var gl = canvas.getContext('webgl');                // The brush and the paints
+
+    var vertexShaderSource = `
+        void main() {
+        }
+    `;
+
+    var fragmentShaderSource = `
+        void main() {
+        }
+    `;
+
+    // Create .c in GPU
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+
+    // Compile .c into .o
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    // Prepare a .exe shell (shader program)
+    var shaderProgram = gl.createProgram();
+
+    // Put the two .o files into the shell
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    // Link the two .o files, so together they can be a runnable program/context.
+    gl.linkProgram(shaderProgram);
+
+    // Start using the context (analogy: start using the paints and the brushes)
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
