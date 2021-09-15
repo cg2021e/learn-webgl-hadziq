2a4a9d96824f857580934ac7b0b05f90dedd04bf
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-15 14:54:07 +0700

    Draw three points in the different locations

--- main.js
+++ main.js
@@ -3,10 +3,29 @@ function main() {
     var canvas = document.getElementById('myCanvas');   // The paper
     var gl = canvas.getContext('webgl');                // The brush and the paints
 
+    // Define vertices data
+    /**
+     * A ( -0.5, -0.5 )
+     * B (  0.5, -0.5 )
+     * C (  0.0,  0.5 )
+     */
+
+    var vertices = [
+        -0.5, -0.5,     // Point A
+         0.5, -0.5,     // Point B
+         0.0,  0.5      // Point C
+    ];
+
+    // Create a linked-list for storing the vertices data
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     var vertexShaderSource = `
+        attribute vec2 aPosition;
         void main() {
             gl_PointSize = 10.0;
-            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);     // Center of the coordinate
+            gl_Position = vec4(aPosition, 0.0, 1.0);     // Center of the coordinate
         }
     `;
 
@@ -39,8 +58,22 @@ function main() {
     // Start using the context (analogy: start using the paints and the brushes)
     gl.useProgram(shaderProgram);
 
+    // Teach the computer how to collect
+    //  the positional values from ARRAY_BUFFER
+    //  to each vertex being processed
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(
+        aPosition, 
+        2, 
+        gl.FLOAT, 
+        false, 
+        0, 
+        0
+    );
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.1, 0.1, 0.1, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
